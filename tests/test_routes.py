296e2384090771335d92ed09cc9181df98a9d59e
Daniel Salin <daniel.salin@gmail.com>
2025-01-31 13:25:11 +0100

    Test:  basic index route test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,4 +1,14 @@
 # TESTING THE ROUTES USING FIXTURES
+from app import App
 
-
-
+def test_home_page():
+    """
+    GIVEN the Flask application
+    WHEN the index route is reached via GET method
+    THEN check that it returns a 200 status code
+    """
+    app = App().create_app()
+    with app.test_client() as test_client:
+        response = test_client.get("/")
+        assert response.status_code == 200
+    
