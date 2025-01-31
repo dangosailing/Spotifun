0b213c49143b13037640087beefedc443e92e983
Daniel Salin <daniel.salin@gmail.com>
2025-01-31 13:43:37 +0100

    Test: Refactor route test to use test_client fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,15 @@
 import pytest
 from app.models import User
+from app import App
 
 @pytest.fixture(scope="module")
 def new_user():
     user = User(username="testUser", password="password123")
     return user
+
+@pytest.fixture(scope="module")
+def test_client():
+    app = App().create_app()
+    with app.test_client() as testing_client:
+        yield testing_client
+    
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,14 +1,9 @@
-# TESTING THE ROUTES USING FIXTURES
-from app import App
-
-def test_home_page():
+def test_home_page(test_client):
     """
     GIVEN the Flask application
     WHEN the index route is reached via GET method
     THEN check that it returns a 200 status code
     """
-    app = App().create_app()
-    with app.test_client() as test_client:
-        response = test_client.get("/")
-        assert response.status_code == 200
+    response = test_client.get("/")
+    assert response.status_code == 200
     
