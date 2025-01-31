652ccbee1166849d888aa5a5b8b61321486c5b42
Daniel Salin <daniel.salin@gmail.com>
2025-01-31 11:51:33 +0100

    Fix app config issue

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,27 @@
+from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
-from config import Config
 from app.routes import bp
+from config import Config
 
+db = SQLAlchemy()
 
 
 class App:
     def ___init__(self):
-        self.config = Config()
+        pass
 
     def create_app(self):
         spotifun_app = Flask(__name__)
-        spotifun_app.config.from_object(self.config)
+        # load our config file into the flask config vars
+        spotifun_app.config.from_object(Config)
+        db.init_app(spotifun_app)
+        # register the blueprint w/ routes into the flask app
         spotifun_app.register_blueprint(bp)
 
+        # setup context from which app can get and use vars and methods
         with spotifun_app.app_context():
+            # import routes and models into the app context
             from . import routes, models
+            db.create_all()
 
         return spotifun_app
